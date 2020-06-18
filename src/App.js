075561c5ae3ec7648e2ad2080a944828479db7d1
Jake O'Toole <otoolej33@gmail.com>
2020-06-18 12:41:37 -0500

    Created new divs, reorganized HTML elements, added additional styling all based on a medium article.

--- src/App.js
+++ src/App.js
@@ -9,20 +9,28 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <div id="app">
-        <Sidenav id="sidenav" />
-        <Switch id="main">
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+      <div className="container">
+        {/* <Sidenav id="sidenav" /> */}
+        <div className="header"></div>
+        <div className="main">
+          <Sidenav className="sidenav" />
+          <div className="content">
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+            <p>Hello</p>
+          </div>
+        </div>
       </div>
+      <div className="footer"></div>
     </Router>
   );
 }
